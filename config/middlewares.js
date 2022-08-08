3054db1d68b3bb2933d45bb036c2099abb4093ce
Aleksandar Milakovic <alexmilakovic24@gmail.com>
2022-08-08 19:27:33 +0200

    enabled cors 3

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,10 +4,11 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
-      header: '*',
-      origin: ['http://localhost:8080']
-    }
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
   },
   'strapi::poweredBy',
   'strapi::logger',
